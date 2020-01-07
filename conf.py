74e1df9a50958a4636d496721fc5981214c06ca2
XTM233 <shenmingxin2000@gmail.com>
2020-01-07 20:32:48 +0800

    修改名称

--- conf.py
+++ conf.py
@@ -19,13 +19,13 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "认清形势，放弃幻想"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+author = "小透明233"
+email = "1134895657@qq.com"
+author_homepage = "https://www.xtm233.github.io"
+description = "没有悲观的权利"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
